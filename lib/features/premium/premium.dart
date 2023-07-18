b68819cee060485254674999b4db33e9ba600b1c
Lokesh Mishra <flutterydev@gmail.com>
2023-07-18 15:54:09 +0530

    created premium files -- to complete

---
+++ lib/features/premium/premium.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PremiumIntro extends StatelessWidget {
+  const PremiumIntro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
